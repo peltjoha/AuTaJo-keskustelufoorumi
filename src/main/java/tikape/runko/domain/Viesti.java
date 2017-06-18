6a41ed7f09bb0655000e6c79d00cfcd7bc426761
tlinna <tlinna@kapsi.fi>
2017-06-19 00:09:03 +0300

    Viestiolio kondikseen

--- src/main/java/tikape/runko/domain/Viesti.java
+++ src/main/java/tikape/runko/domain/Viesti.java
@@ -28,21 +28,17 @@ public class Viesti {
         this.kayttaja = kayttaja;
         this.viestiketju = viestiketju;
     }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getTimestamp() {
-<<<<<<< HEAD
-
         return timestamp; // palautetaan tässä vain olemassaoleva aikaleima
-=======
-        
-        // palauttaa kyselyhetken aikaleiman eikä viestin?
-        
-        // Saattaa olla, tää on suoraan sanottuna vähän tällasta trial & error meininkiä mulla
-        // Ootko kartalla, miten tää pitäis käytännössä toteuttaa? -J
-        
-        String timestamp = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
-        return timestamp;
->>>>>>> 216861ba8f56d5e021ff5281afc2a31118e2018c
     }
 
     public void setTimestamp(String timestamp) {
